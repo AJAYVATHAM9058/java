c5ade87e94eed694e120e2da1d759f455bab2b19
AJAYVATHAM9058 <75926757+AJAYVATHAM9058@users.noreply.github.com>
2021-02-17 10:48:17 +0530

    Create whobecomemillionaire.java

---
+++ whobecomemillionaire.java
@@ -0,0 +1,19 @@
+public class WhoBecomeMillionaire
+{
+	public static void main(String[] args) {
+		
+		char answer = 'C';
+		
+		System.out.println("Who is named as inventor of light bulb");
+		System.out.println("Your marked answer is "+ answer+". Let's see wether you are right");
+		
+		if(answer=='C'){
+		    System.out.println("You were right.You won €1.");
+		}
+		else{
+		    System.out.println("Sorry,the right answer is Tomas Edison.Thanks For coming");
+		}
+		
+		
+	}
+}
